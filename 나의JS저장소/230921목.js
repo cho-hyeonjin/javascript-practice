6176829f8c6875847e6c709802df108e087bb42c
cho-hyeonjin <gusls2005@gmail.com>
2023-09-22 15:44:16 +0900

    실행 컨텍스트_코드 평가와 실행  과정 공부중

--- 나의JS저장소/230921목.js
+++ 나의JS저장소/230921목.js
@@ -1,20 +1,26 @@
 // 소스코드의 평가와 실행
-var x;
-x = 1;
+// var x;
+
+// x = 1;
 
 // 실행 컨텍스트의 역할
-const x = 1;
-const y = 2;
 
-function foo(a) {
-  const x = 10;
-  const y = 20;
-  console.log(a + x + y);
-}
+// const x = 1;
 
-foo(100);
+// const y = 2;
 
-console.log(x + y);
+// function foo(a) {
+
+//     const x = 10;
+
+//     const y = 20;
+
+//     console.log(a + x + y);
+// }
+
+// foo(100);
+
+// console.log(x + y);
 
 // 소스코드 평가와 실행 순서
 // 1. 전역코드 평가. 평가 시 변수나 함수 선언문은 먼저 실행 컨텍스트가 관리하는 스코프* 에 등록된다.
@@ -27,3 +33,19 @@ console.log(x + y);
 //    > 전역코드에서와 마찬가지로 함수 안에서 매개변수나 지역변수 선언문이 있는 경우 선언문을 먼저 실행하여 실행 컨텍스트가 관리하는 지역스코프에 등록된다.
 // 4. 함수 코드 실행
 // 4-1. 함수 안에서 상위 스코프에 있는 변수를 참조한 경우 스코프 체인을 통해 검색, 실행
+
+const x = 1;
+
+function foo() {
+  const y = 2;
+
+  function bar() {
+    const z = 3;
+
+    console.log(x + y + z);
+  }
+
+  bar();
+}
+
+foo();
